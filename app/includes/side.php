15ac6ba7bb13d594027ef61a0b75abb4f6f8880e
emrahhcan <emrahcansahinoglu@gmail.com>
2021-08-05 00:43:04 +0300

    Fetching posts and searching functionality has been added

--- app/includes/side.php
+++ app/includes/side.php
@@ -1,24 +1,29 @@
 <div class="col-lg-4">
     <!-- Search widget-->
     <div class="card mb-4">
-        <div class="card-header">Search</div>
+        <div class="card-header">
+            <h5 class="mainTextColor">Search</h5>
+        </div>
         <div class="card-body">
-            <div class="input-group">
-                <input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
-                <button class="btn btn-primary" id="button-search" type="button">Go!</button>
-            </div>
+            <form action="blog.php" method="post">
+                <div class="input-group">
+                    <input class="form-control" name="search-term" type="text" placeholder="Enter search term..." aria-label="Enter search term..." />
+                </div>
+            </form>
         </div>
     </div>
     <!-- Categories widget-->
     <div class="card mb-4">
-        <div class="card-header">Tags</div>
+        <div class="card-header">
+            <h5 class="mainTextColor">Tags</h5>
+        </div>
         <div class="card-body">
             <div class="row">
                 <div class="col-sm-6">
                     <ul class="list-unstyled mb-0">
                         <?php foreach($tags as $key => $tag): ?>
                             <li>
-                                <a href="#!">
+                                <a href="#!" class="blogTags">
                                     <?php echo ucwords($tag['name']); ?>
                                 </a>
                             </li>
--- blog.php
+++ blog.php
@@ -1,6 +1,16 @@
 <?php 
     include('path.php');
     include(APPROOT . '/app/controllers/tags.php');
+    
+    $posts = array();
+    $blogPageTitle = 'Recent Articles';
+
+    if(isset($_POST['search-term'])) {
+        $blogPageTitle = 'You are looking for ' . '"' . $_POST['search-term'] . '"';
+        $posts = searchPosts($_POST['search-term']);
+    } else {
+        $posts = getPublishedPosts();
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,14 +31,16 @@
         <?php include(APPROOT . '/app/includes/messages.php'); ?>
             <header class="blog-img py-5 bg-light border-bottom mb-4">
                 <div class="container">
-                    <div class="text-center my-5">
-                        <h1 class="fw-bolder">Welcome to My Blog</h1>
+                    <div class="text-center my-5 mainTextColor">
+                        <h1 class="fw-bolder">Welcome to Cindy's Life</h1>
                         <div class="tags-divider">
                             <div class="tags-divider-line"></div>
                             <div class="tags-divider-icon"><i class="fas fa-star"></i></div>
                             <div class="tags-divider-line"></div>
                         </div>
-                        <p class="lead mb-0">Let's Get Started</p>
+                        <p class="lead mb-0">
+                            <h3><?php echo $blogPageTitle; ?></h3>
+                        </p>
                     </div>
                 </div>
             </header>
@@ -37,15 +49,23 @@
                 <!-- Blog entries-->
                 <div class="col-lg-8">
                     <!-- Featured blog post-->
-                    <div class="card mb-4">
-                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
-                        <div class="card-body">
-                            <div class="small text-muted">January 1, 2021</div>
-                            <h2 class="card-title">Featured Post Title</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                            <a class="btn btn-primary" href="#!">Read more →</a>
+                    <?php foreach($posts as $post): ?>
+                        <div class="card mb-4">
+                            <a href="#!"><img class="card-img-top" src="<?php echo URLROOT . '/assets/img/postImages/' . $post['image']; ?>" alt="..." /></a>
+                            <div class="card-body">
+                                <div class="small text-muted">
+                                    <?php echo date('F j, Y', strtotime($post['created_at'])); ?> 
+                                    by
+                                    <?php echo ucwords($post['username']); ?>
+                                </div>
+                                <h2 class="card-title"><?php echo $post['title']; ?></h2>
+                                <p class="card-text">
+                                    <?php echo html_entity_decode(substr($post['body'], 0, 250) . '...'); ?>
+                                </p>
+                                <a class="btn btn-primary" href="#!">Read More</a>
+                            </div>
                         </div>
-                    </div>
+                    <?php endforeach; ?>
                 </div>
                 <!-- Side widgets-->
                 <?php include(APPROOT . '/app/includes/side.php'); ?>
